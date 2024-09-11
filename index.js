896e8a8296838b4a69016d5ddbb31543ff297fd9
bhargavvijay <141197189+bhargavvijay@users.noreply.github.com>
2024-09-11 20:39:40 +0530

    Update index.js

--- index.js
+++ index.js
@@ -14,6 +14,17 @@ app.use(cors({
 
 app.use(express.json());
 
+// Function to create alpha3Code to country name dictionary
+const createAlpha3CodeToNameMap = (data) => {
+    const map = {};
+    data.forEach(country => {
+        if (country.alpha3Code && country.name) {
+            map[country.alpha3Code] = country.name;
+        }
+    });
+    return map;
+};
+
 app.get('/', (req, res) => {
     const search = req.query.search;
     const page = parseInt(req.query.page) || 0;
@@ -27,6 +38,9 @@ app.get('/', (req, res) => {
         
         let data1 = JSON.parse(data);
         let filteredData = data1;
+
+        // Create the alpha3Code to name map from the data
+        const alpha3CodeToName = createAlpha3CodeToNameMap(data1);
 
         // If search parameter is present, filter by country name
         if (search) {
@@ -44,6 +58,14 @@ app.get('/', (req, res) => {
             return res.status(404).json({ message: 'No such country found' });
         }
 
+        // Convert borders alpha3Codes to country names
+        filteredData = filteredData.map(country => {
+            if (country.borders) {
+                country.borders = country.borders.map(code => alpha3CodeToName[code] || code);
+            }
+            return country;
+        });
+
         // Pagination
         const start = page * perPage;
         const end = start + perPage;
@@ -59,3 +81,5 @@ app.get('/', (req, res) => {
 app.listen(PORT, () => {
     console.log(`Server running at http://localhost:${PORT}/`);
 });
+
+    
